dbdc515c1529bf602bddd06769d3492f7b699969
Primocode <@gmail.com>
2020-08-13 14:49:14 +0200

    endments

--- quiz.js
+++ quiz.js
@@ -50,26 +50,47 @@ document.querySelector('.showResult').addEventListener('click', calculation);
 
 const prevailingAttitude = (reactive, manipulated, aggressive, assertive) => {
 
-    let attitude = "Twoja przeważająca postawa to ";
-    let features = "Wówczas można Ci przypisać pewne z poniższych cech ";
+
+
     // `${reactive} ${manipulated} ${aggressive} ${assertive}`;
 
     // document.querySelector('.prevailing-attitude-result').textContent = Math.max(reactive, manipulated, aggressive, assertive);
 
-    if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-bierny').textContent) {
-        document.querySelector('.prevailing-attitude-result').textContent = attitude + "bierny"
-        document.querySelector('.features').textContent = features + `brak pewności siebie i niska samoocena<br> •brak szacunku do samego siebie<br> poniżanie samego siebie<br>negatywne uczucia i myśli na temat własnej osoby`
+    if (Math.max(reactive, manipulated, aggressive, assertive) >= 1) {
+        if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-bierny').textContent) {
+            const attitude = "Bierność"
+            const features = `<br>• brak pewności siebie i niska samoocena <br> • brak szacunku do samego siebie<br> • poniżanie samego siebie<br>• negatywne uczucia i myśli na temat własnej osoby`
+            valueSupplementation(attitude, features)
+        }
+    
+        if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-manipulujacy').textContent) {
+            const attitude = "manipulujący"
+            const features = `<br>• brak pewności siebie i niska samoocena<br>• brak szacunku do siebie oraz innych ludzi<br>• podejrzliwość i niedowierzanie w stosunku do motywów działań otoczenia<br>• negatywne myśli i uczucia na temat innych osób oraz własnego „ja”<br>• duża ostrożność w stosunku do innych<br>• nieuczciwość i brak szczerości<br>• zniekształcanie znaczeń cudzych wypowiedzi<br>• podawanie w wątpliwość poczucia godności innych ludzi<br>• depresja i brak motywacji`
+            valueSupplementation(attitude, features)
+        }
+    
+        if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-agresywny').textContent) {
+            const attitude = "agresywny"
+            const features = `<br>• brak pewności siebie i niska samoocena<br>• brak szacunku dla innych<br>• lekceważenie innych<br>• poczucie wyższości<br>• chęć kontrolowania otoczenia i sytuacji<br>• brak zainteresowania cudzymi uczuciami i myślami<br>
+            • złość w stosunku do innych ludzi i skłonność do obwiniania ich<br>• nieumiejętność słuchania pytań i zadawania ich<br>• ignorowanie reakcji otoczenia`
+            valueSupplementation(attitude, features)
+        }
+    
+        if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-asertywny').textContent) {
+            const attitude = "asertywność";
+            const features = `<br>• pewność siebie i wysoka samoocena<br>• szacunek dla samego siebie oraz innych<br>• odpowiedzialność za siebie<br>• motywacja do dobrej pracy<br>• zainteresowanie uczuciami oraz myślami innych ludzi<br>• umiejętność zadawania pytań<br>• uczciwość i bezpośredniość<br>• umiejętność słuchania innych <br>• potrzeba poznania reakcji otoczenia`
+            valueSupplementation(attitude, features)
+        }
     }
+    else {
+        document.querySelector('.features').textContent = "Musisz najpierw uzupełnić kwestionariusz"
+    }
+}
 
-    if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-manipulujacy').textContent) {
-        document.querySelector('.prevailing-attitude-result').textContent = attitude + "manipulujący"
-    }
+const valueSupplementation = (attitudeValue, featuresValue) => {
+    let attitude = "Twoja przeważająca postawa to ";
+    let features = `<p class=underthetitle>Wówczas można Ci przypisać pewne z poniższych cech</p>`;
 
-    if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-agresywny').textContent) {
-        document.querySelector('.prevailing-attitude-result').textContent = attitude + "agresywny"
-    }
-
-    if (Math.max(reactive, manipulated, aggressive, assertive) == document.querySelector('.result-asertywny').textContent) {
-        document.querySelector('.prevailing-attitude-result').textContent = attitude + "asertywny"
-    }
+    document.querySelector('.prevailing-attitude-result').textContent = attitude + attitudeValue;
+    document.querySelector('.features').innerHTML = features + featuresValue;
 }
